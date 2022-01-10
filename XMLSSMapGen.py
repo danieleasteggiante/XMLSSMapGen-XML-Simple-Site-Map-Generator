392049e5bdc254f040e1ccdbd29a3ea286a0aa4a
danieleasteggiante <52632176+danieleasteggiante@users.noreply.github.com>
2022-01-10 10:24:02 +0100

    Update XMLSSMapGen.py

--- XMLSSMapGen.py
+++ XMLSSMapGen.py
@@ -1,73 +1,101 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import threading
+         
+SITE_MAP = []
 
-def cercaUrl(url, filtro, stringUrl):
-    links= []
-    httpUrl = url.replace("https","http")
+def parser(htmlRaw):
+    soup= BeautifulSoup(htmlRaw,"html.parser")
+    a_list = soup.findAll('a', href=True)
+    return a_list
+
+""" def extractLinks(a_list, domain, base_url):
+    link_list = []
+    for a in a_list:
+        href = a.get('href')
+        if len(href)> 1 and href[-1] == '/':
+            href = href[:-1]            
+        if domain in href:
+            link_list.append(href)
+        elif domain not in href:
+            if "http" not in href:
+                if href.startswith('/'):
+                    link_list.append(base_url + href)
+                elif re.match(r'^[a-z]',href):
+                    link_list.append(base_url + "/" + href)
+                elif re.match(r'^tel',href):
+                    link_list.append(base_url + "/" + href)
+                elif re.match(r'#',href):
+                   continue
+    return link_list """
+
+def extractLinks(a_list, domain, base_url):
+    for a in a_list:
+        href = a.get('href')
+        if len(href)> 1 and href[-1] == '/':
+            href = href[:-1]            
+        if domain in href:
+            yield href
+        elif domain not in href:
+            if "http" not in href:
+                if href.startswith('/'):
+                    yield base_url + href
+                elif re.match(r'^[a-z]',href):
+                   yield base_url + "/" + href
+                elif re.match(r'^tel',href):
+                   yield base_url + "/" + href
+                elif re.match(r'#',href):
+                   continue
+
+
+def spider(url, domain, baseurl):
+    global SITE_MAP
+    httpUrl = url.replace("https","http") 
     try:
         req =  requests.get(url)
     except:
-        req =  requests.get(httpUrl)
-    content = req.content
+        req =  requests.get(httpUrl)     
+    content = req.content  
+    a_list = parser(content)
+    #site_map_tmp = list(set(extractLinks(a_list, domain, baseurl)))
+    for elem in extractLinks(a_list, domain, baseurl):
+        if elem not in SITE_MAP:
+            SITE_MAP.append(elem)
 
-    soup= BeautifulSoup(content,"html.parser")
+def index_in_list(index, a_list):
+    if index < len(a_list):
+        return True
+    else:
+        return False
+    
+def exploreSite(url):
+    global SITE_MAP
+    domain = url.split("//")[1]
+    SITE_MAP.append(url)
+    threads=[]
+    counter=0
+    spider(SITE_MAP[0],domain,url)
+    SITE_MAP = list(set(SITE_MAP))
+    flag= True
+    while flag:
+        print('\rAbbiamo controllato {0} links su {1} trovati'.format(counter, len(SITE_MAP)), end='', flush=True)
+        
+        for i in range(0,10):
+            flag = index_in_list(counter + i, SITE_MAP)
+            if flag:
+                worker = threading.Thread(target=spider, args=[SITE_MAP[counter+i], domain, url])
+                threads.append(worker)
+                worker.start()
+                counter+=1
+            else:
+               flag = False 
+        
+        for el in threads:
+            el.join()
+    return SITE_MAP
 
- 
-    for link in soup.findAll('a', href=True):
-        try:
-            if filtro in link.get('href'):
-                links.append(link.get('href'))
-            elif filtro not in link.get('href'):
-                if "http" not in link.get('href'):
-                    if link.get('href').startswith('/'):
-                        links.append(stringUrl + link.get('href'))
-                    elif re.match(r'^[a-z]',link.get('href')):
-                        links.append(stringUrl + "/" + link.get('href'))
-                    elif re.match(r'^tel',link.get('href')):
-                        links.append(stringUrl + "/" + link.get('href'))
-                    elif re.match(r'#',link.get('href')):
-                        continue
-                    
-        except:
-            print('error')
-            print(link)
-            continue
-
-
-    return links
-
-def cercaUrl2(lista, filtro, stringUrl):
-    lista_url=[]
-    lista_url.extend(lista)
-    gia_controllati=[]
-    url_finali = []
-    ind = -1
-    while len(gia_controllati) < len(lista_url):
-        gia_controllati.append(lista_url[ind])
-        ind = ind + 1
-        print('gia controllati: ')
-        print(len(gia_controllati))
-         
-        if lista_url[ind] in gia_controllati:
-            print('ce----------------' + lista_url[ind] )
-        else:
-            try:
-                new_link = cercaUrl(lista_url[ind],filtro, stringUrl)
-                url_finali.append(lista_url[ind])
-                print(len(lista_url))
-                print(lista_url[ind])
-                for i in new_link:
-                    if i in lista_url:
-                        print('gia inserito')
-                    else:
-                        lista_url.append(i)
-            except TypeError:
-                pass
-
-    return url_finali
-
-def stampaXml(elenco_url, nome_file):
+def stampaXml(elenco_url, nome_file="site_map"):
     with open (nome_file + ".xml", "w") as fileXml:
         fileXml.write('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
 
@@ -82,10 +110,23 @@ def stampaXml(elenco_url, nome_file):
         fileXml.write('</urlset>\n')
         print('Fine processo')
 
-url = input('inserire url:')
-filtro = url.split("//")[1]
-stringUrl = url
-elenco_home = cercaUrl(url,filtro, stringUrl)
-elenco_sito = cercaUrl2(elenco_home,filtro, stringUrl)
-print('--------------')
-stampaXml(elenco_sito,filtro)
+if __name__ == '__main__':
+    logo = """
+     __   ____  __ _       _____ _ _       __  __          _____   _____            
+ \ \ / /  \/  | |     / ____(_) |     |  \/  |   /\   |  __ \ / ____|           
+  \ V /| \  / | |    | (___  _| |_ ___| \  / |  /  \  | |__) | |  __  ___ _ __  
+   > < | |\/| | |     \___ \| | __/ _ \ |\/| | / /\ \ |  ___/| | |_ |/ _ \ '_ \ 
+  / . \| |  | | |____ ____) | | ||  __/ |  | |/ ____ \| |    | |__| |  __/ | | |
+ /_/ \_\_|  |_|______|_____/|_|\__\___|_|  |_/_/    \_\_|     \_____|\___|_| |_|
+                                                                                                                                                        
+    
+    """
+    print(logo)
+    url = ''
+    while len(url) < 14:
+        if len(url) < 14:
+            url= input('Inserire nome valido (es.: https://www.nomesito.it): ')
+    file_name = input('inserire nome del file da salvare: ')
+
+    siteMap = exploreSite(url)
+    stampaXml(siteMap)
